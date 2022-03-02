7842048c3f6db4cc431f6649444e651dd247b914
msvajgl <msvajgl@gmail.com>
2022-03-02 16:25:12 +0100

    GreatCart-level12

--- store/models.py
+++ store/models.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 
 # Create your models here.
 
-class Product (models.Model):
+class Product(models.Model):
     product_name    = models.CharField(max_length=200, unique=True)
     slug            = models.SlugField(max_length=200, unique=True)
-    description     = models.TextField(max_length=200, blank=True)
+    description     = models.TextField(max_length=500, blank=True)
     price           = models.IntegerField()
     images          = models.ImageField(upload_to='photos/products')
     stock           = models.IntegerField()
@@ -36,7 +36,7 @@ variation_category_choice = (
 
 class Variation(models.Model):
     product             = models.ForeignKey(Product, on_delete=models.CASCADE)
-    variation_category  = models.CharField(max_length=200, choices=variation_category_choice)
+    variation_category  = models.CharField(max_length=100, choices=variation_category_choice)
     variation_value     = models.CharField(max_length=100)
     is_active           = models.BooleanField(default=True)
     created_date        = models.DateTimeField(auto_now=True)
