bc552396f937393afe034f1a5441af6372fb65b0
msvajgl <msvajgl@gmail.com>
2022-03-31 12:19:26 +0200

    GreatCart lvl.22

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from .models import Product, ReviewRating
 from category.models import Category
 from carts.models import CartItem
 from django.db.models import Q
+from orders.models import OrderProduct
 
 from carts.views import _cart_id
 from django.http import HttpResponse
@@ -46,9 +47,23 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
+
     context = {
         'single_product': single_product,
         'in_cart'       : in_cart,
+        'orderproduct'  : orderproduct,
+        'reviews'       : reviews,
     }
     return render (request, 'store/product_detail.html', context)
 
